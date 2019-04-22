044d56c3cbf011ee4447a1802ba7fdfafdea760b
Ivan Andreyko <myromeu@ya.ru>
2019-04-22 19:52:31 +0300

    missed dirmngr in mongo script

--- shell_scripts.py
+++ shell_scripts.py
@@ -1,4 +1,6 @@
 MONGO_SCRIPT = '''
+apt-get install dirmngr -y
+
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
 
 echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/3.6 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
