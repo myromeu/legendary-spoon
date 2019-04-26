b823ac460a9df364166f1b49ee9bc8bbbe50e73a
Ivan Andreyko <myromeu@ya.ru>
2019-04-27 00:00:23 +0300

    handle replication arg and install primary&secondary 3 instance mongodb

--- shell_scripts.py
+++ shell_scripts.py
@@ -16,6 +16,42 @@ echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
 echo "mongodb-org-tools hold" | sudo dpkg --set-selections
 '''
 
+MONGO_REPLICA_SET_SCRIPT = '''
+apt-get install dirmngr wget -y
+
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+
+echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/3.6 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+
+apt-get update
+
+apt-get install mongodb-org -y
+
+echo "mongodb-org hold" | sudo dpkg --set-selections
+echo "mongodb-org-server hold" | sudo dpkg --set-selections
+echo "mongodb-org-shell hold" | sudo dpkg --set-selections
+echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+
+mkdir -p /data/mongodb/one /data/mongodb/two /data/mongodb/three
+chmod 755 /data
+chown -R mongodb:mongodb /data/mongodb
+mkdir -p /var/log/mongodb/one /var/log/mongodb/two /var/log/mongodb/three
+chown -R mongodb:mongodb /var/log/mongodb
+sudo -u mongodb -g mongodb mongod --replSet rs0 --port 27017 --dbpath /data/mongodb/one --fork --logpath /var/log/mongodb/one/mongod.log
+sudo -u mongodb -g mongodb mongod --replSet rs0 --port 27027 --dbpath /data/mongodb/two --fork --logpath /var/log/mongodb/two/mongod.log
+sudo -u mongodb -g mongodb mongod --replSet rs0 --port 27037 --dbpath /data/mongodb/three --fork --logpath /var/log/mongodb/three/mongod.log
+sleep 5
+
+mongo --port 27017 <<EOF
+    rs.initiate({_id: "rs0", members: [
+        {_id: 0, host: "localhost:27017"},
+        {_id: 1, host: "localhost:27027"},
+        {_id: 2, host: "localhost:27037"}
+    ], settings: {electionTimeoutMillis: 2000}});
+EOF
+'''
+
 RABBIT_SCRIPT = '''
 apt-get install dirmngr wget -y
 
